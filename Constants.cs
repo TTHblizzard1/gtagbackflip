62c4bc4a76f9df072b00689d3426864d4f2a0d42
TTHblizzard1 <168460896+TTHblizzard1@users.noreply.github.com>
2026-07-06 03:11:08 -0400

    Create Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,8 @@
+    public const string Guid = "struct.backflip";
+
+    internal const float PressWindow = 0.4f;
+    internal const float MinFlipDuration = 0.05f;
+    internal const float FlipDuration = 0.4f;
+    internal const float MinCooldown = 0.05f;
+    internal const float MaxCooldown = 3f;
+}
